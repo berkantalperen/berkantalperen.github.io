c52dce5e5187465c24edec2a607c6c6eadf30c27
berkantalperen <berkantalperen@outlook.com>
2020-07-03 20:10:31 +0300

    woosh

--- berkant.js
+++ berkant.js
@@ -1,8 +1,7 @@
 (function(){
-   /*var paragraphs=document.getElementsByTagName('p');
+   var paragraphs=document.getElementsByTagName('p');
     for(i=0;i<paragraphs.length;i++){
         paragraphs[i].innerHTML="woossshh"
-    }*/
+    }
 
-    alert('a');
 })();
