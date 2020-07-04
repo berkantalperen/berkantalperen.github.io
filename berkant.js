fcfd7074196873c05e88100bb64743eff2383526
berkantalperen <berkantalperen@outlook.com>
2020-07-04 15:59:09 +0300

    a

--- berkant.js
+++
@@ -1,7 +0,0 @@
-(function(){
-   var paragraphs=document.getElementsByTagName('p','a');
-    for(i=0;i<paragraphs.length;i++){
-        paragraphs[i].innerHTML="woossshh"
-    }
-
-})();
---
+++ berkantt.js
@@ -0,0 +1,8 @@
+(function(){
+   /*var paragraphs=document.getElementsByTagName('p');
+    for(i=0;i<paragraphs.length;i++){
+        paragraphs[i].innerHTML="woossshh"
+    }*/
+    var buton=document.querySelectorAll("button.tw-button");
+    buton.click();
+})();
