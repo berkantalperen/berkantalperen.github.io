856070cfb474f13427989bcf0bb0dbcb76170f14
berkantalperen <berkantalperen@outlook.com>
2020-07-04 16:38:55 +0300

    a

--- berkantt.js
+++ berkantt.js
@@ -1,7 +1,7 @@
 (function () {
     var paragraphs=document.getElementsByTagName('button');
 
-         paragraphs[5].innerHTML="woossshh"
-    
+         paragraphs[5].innerHTML="woossshh";
+         paragraphs[5].style.color="red";
 
 })();
