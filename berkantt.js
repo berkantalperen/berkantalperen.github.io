61de2c954ce5dbe9ba6fd263173af24b8917e999
berkantalperen <berkantalperen@outlook.com>
2020-07-04 16:51:29 +0300

    a

--- berkantt.js
+++ berkantt.js
@@ -2,6 +2,6 @@
     var paragraphs=document.getElementsByTagName('button');
         console.log('a');
          paragraphs[16].innerHTML="woossshh";
-         paragraphs[16].style.color="red";
+         paragraphs[16].style.size="3px";
 
 })();
